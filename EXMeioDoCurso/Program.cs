51b8726dce621ead215a4da12da2d44a45785069
Miguel <miguelgsilverios@gmail.com>
2022-02-15 17:32:25 -0300

    Exercicios S2E1 e S4E1

---
+++ EXMeioDoCurso/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXMeioDoCurso
+{
+    public static class Program
+    {
+        public static void Main(String[] args)
+        {
+            // S2E1
+            string nome;
+            int idade;
+            int dataAtual = DateTime.Now.Year;
+            // DateTime.Now.ToString("Y");
+
+            Console.Write("Favor insira o seu nome: ");
+            nome = Console.ReadLine();
+            Console.WriteLine();
+
+            Console.Write("Favor insira a sua idade: ");
+            idade = Int32.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.WriteLine(nome);
+            Console.WriteLine(idade);
+            Console.WriteLine("Seu ano de nascimento e: " + (dataAtual - idade) +
+                " ou " + (dataAtual - idade - 1));
+
+            // S4E1
+            int x = matematica.exponenciacao(2, 5);
+            Console.WriteLine(String.Format("O resultado da potencia e: {0}", x));
+        }
+    }
+}
---
+++ EXMeioDoCurso/matematica.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXMeioDoCurso
+{
+    class matematica
+    {
+        public static int exponenciacao(int a, int b)
+        {
+            int resultado = 1;
+            if (b != 0)
+            {
+                for (int i = 1; i <= b; i++)
+                {
+                    resultado = resultado * a;
+                }
+            }
+            return resultado;
+        }
+    }
+}
